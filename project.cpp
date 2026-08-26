8dbbd37bce2bb8434cc99aa67133e32c4d105518
Widad-Khalil <widadkhalil32@gmail.com>
2026-08-27 02:06:34 +0300

    first commit

---
+++ project.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+struct Course {
+    string code;
+    string name;
+    int credits;
+    int value;
+    string prereq;
+};
+
+struct Student{
+    int id;
+    string name;
+    double gpa;
+};
+
+int seed = 0;
+int num_courses = 0;
+int num_records = 0;
+int credit_cap = 0;
+
+vector<Course> courses;
+vector<Student> records;
+
+unsigned int lcg_next(unsigned int &x){
+    unsigned int next_val = (1103515245ULL * x + 12345ULL) % 2147483648ULL;
+    x = next_val;
+    return next_val;
+}
+
+void generateData(int student_id){
+    seed = student_id % 10000;
+    num_courses = 12 + (seed % 6);
+    num_records = 40 + (seed % 20);
+    credit_cap = 15 + (seed % 4);
+     unsigned int current_seed = static_cast<unsigned int>(seed);
+     string course_prefixes[] = {"ENG" , "CS" , "MATH" , "PHY"};
+     string course_names[] = {"Data Struct" , "Discrete" , "Algorithms" , "Digital Logic" ,
+                             "Circuits" , "Intro Programming" , "Linear Algebra" , "Signals" ,
+                              "OS" , "DB Systems" , "Networks" , "Web Dev" , "AI Intro" , 
+                            "Cybersecurity" , "Embeded Sys" , "Software Eng" ,"Microcontrollers" , "VLSI Design" };
+    
+    courses.clear();
+
+    for (int i = 0; i < num_courses; ++i) {
+        Course c;
+        c.code = course_prefixes[i % 4] + to_string(101 + i * 10);
+        c.name = course_names[i % 18];
+        c.credits = 2 + (lcg_next(current_seed) % 3);
+        c.value = 5 + (lcg_next(current_seed) % 6);
+        
+    if (i == 0 || (lcg_next(current_seed) % 3 == 0)) {
+        c.prereq = "-";
+    } else {
+      int prereq_idx = lcg_next(current_seed) % i;
+      c.prereq = courses[prereq_idx].code;  
+    }
+    courses.push_back(c);
+    }                        
+
+    string first_names[] = {"Lia" , "Sara" , "Yara" , "Ahmad" , "Samer" , "Adam" , "Lina" , "Tariq" , "Nour" , " Naya"};
+    string last_names[] = { "Yousef" , "Saleh" , "Nasser" , "Qasim" , "Fares" , "Masri" , "kareem" , "Hamad" , "Sami" ,"Waseem"};
+    records.clear();
+    for (int i = 0; i < num_records; ++i) {
+        Student s;
+        s.id = 2025000 + (lcg_next(current_seed) % 900);
+        s.name = first_names[lcg_next(current_seed) % 10] + " " +last_names[lcg_next(current_seed) % 10];
+        s.gpa = 2.0 + (lcg_next(current_seed) % 201) / 100.0; // GPA between 2.00 and 4.00
+        records.push_back(s);
+    }
+}
